ccd7b1ae370ef92d064a37222d504a46dedccddd
listerineh <sebask8er.alvarez@gmail.com>
2025-07-31 00:08:17 -0500

    feat: remove unused head tag for SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -72,7 +72,6 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <head>
-        <link rel="manifest" href="/manifest.json" />
         <link rel="icon" href="/icon.svg" type="image/svg+xml" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
